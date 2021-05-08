d0943bf7b7bbe1502706f6111f93ee7b3c1d0929
Hoyame <zou.hoyame@gmail.com>
2021-05-09 01:01:08 +0200

    init

---
+++ [gamemode]/sunwise/fxmanifest.lua
@@ -0,0 +1,6 @@
+fx_version 'bodacious'
+game 'gta5'
+
+
+client_script 'dist/client/*.client.js'
+server_script 'dist/server/*.server.js'
---
+++ [gamemode]/sunwise/src/client/client.ts
@@ -0,0 +1,9 @@
+setImmediate(() => {
+  emitNet('helloserver');
+});
+
+onNet('helloclient', message => {
+  console.log(`The server replied: ${message}`);
+});
+
+console.log("z")
