0eaa949559a38aae4a297e336f71505409b39795
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2022-04-25 00:30:19 +0200

    clear project

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,13 +4,10 @@ game 'common'
 lua54 'yes'
 
 client_scripts {
-    'core/cl_c2.lua',
-    'core/client.lua',
     'dist/client/*.js',
 }
 
 server_scripts {
-    'core/server.lua',
     'dist/server/*.js',
 }
 
