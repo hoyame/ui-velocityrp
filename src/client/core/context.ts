32e4d2efd83337d5b0a72c8d5500e82cf6c94104
hoyame <zou.hoyame@gmail.com>
2022-04-25 14:19:33 +0200

    init: context

---
+++ src/client/core/context.ts
@@ -0,0 +1,11 @@
+
+
+export abstract class Context {
+    public static async initialize() {
+        console.log("[Context] Initialized");
+    }
+
+    
+
+    
+}
--- src/client/index.ts
+++ src/client/index.ts
@@ -1,10 +1,12 @@
 import "../shared/extensions";
 
+import { Context } from "./core/context";
+
 class Gamemode {
 	public static async Initialize() {
 		// FreezeEntityPosition(PlayerPedId(), false);
 
-		
+		Context.initialize()
 
 		console.log("GAMEMODE INITIALIZED");
 	}
