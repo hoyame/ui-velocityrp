b36abb6b9d66a27ca945fe53046abb948210a399
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2022-06-05 13:58:30 +0000

    Pending changes exported from your codespace

--- src/client/modules/misc/overlay.ts
+++ src/client/modules/misc/overlay.ts
@@ -10,12 +10,23 @@ interface IOverlay {
     dirty?: number;
 }
 
+interface IStaffData {
+    state: boolean;
+    reportsCours: number;
+    reportsAttente: number;
+}
+
 export abstract class Overlay {
     public static async initialize() {
         onNet('hoyame:updateOverlay', this.update.bind(this))
+        onNet('hoyame:updateStaff', this.updateStaff.bind(this))
     }
 
     public static update(data: IOverlay) {
         SendNuiMessage(JSON.stringify({ type: "overlay", data: data, dark: (GetClockHours() < 21 && GetClockHours() > 6)}));
+    }
+
+    public static updateStaff(data: IStaffData) {
+        SendNuiMessage(JSON.stringify({ type: "staff", data: data }));
     }
 }
